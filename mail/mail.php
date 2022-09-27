98975ffdfc47175a360655bd8230956bb0a3ee31
Nelitha Vindinu Priyawansha <89658553+nelitha14@users.noreply.github.com>
2022-09-27 20:38:01 +0530

    mail error fixed

--- mail/mail.php
+++ mail/mail.php
@@ -18,7 +18,7 @@ try {
     $mail->Host       = "smtp.gmail.com";                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = "nelithavindinu7@gmail.com";                     //SMTP username
-    $mail->Password   = "nelith2006";                               //SMTP password
+    $mail->Password   = "gzggityzvfnlchpl";                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
